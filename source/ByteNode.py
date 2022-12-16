1810313b8439bd27be334d1ee4ca115c65077bc1
Yuval Ritsker <yuvalritsker@gmail.com>
2022-12-16 23:29:20 +0200

    constructor

---
+++ source/ByteNode.py
@@ -0,0 +1,51 @@
+
+class ByteNode:
+    def __init__(self, byte: str):
+        '''
+        Construction function
+        '''
+        if not isinstance(byte, str):
+            raise ValueError("Error! byte must be of type string")
+        if len(byte) != 8:
+            raise ValueError("Error! byte length must be eight")
+        for c in byte:
+            if c != '1' and c != '0':
+                raise ValueError("Error! byte inculdes only '0' or '1'")
+        self.byte = byte
+        self.next = None
+    
+    def get_byte(self):
+        '''
+        A function that returns 'byte' field.
+        '''
+        return self.byte
+
+    def get_next(self):
+        '''
+        A function that returns 'next' field.
+        '''
+        return self.next
+
+    def set_next(self, next):
+        '''
+        A function that updates 'next' field into next parameter.
+        '''
+        self.next = next
+    
+    def __repr__(self):
+        return f"[{self.byte}]=>"
+
+if __name__ == "__main__":
+    # bn = ByteNode(byte='10011000')
+    # print(bn)
+    # bn2 = ByteNode('11111111')
+    # bn2.set_next(bn)
+    # print(bn2.get_next().get_byte())
+    binary = bin(256)[2:]
+    remnant = len(binary) % 8
+    my_str = bin(256)[2:].zfill(((8 - remnant) + len(binary)))
+    head = ByteNode(byte = my_str[:8])
+    #print(binary)
+    #print(remnant)
+    #print(my_str)
+    print(head)
--- source/LinkedListBinaryNum.py
+++ source/LinkedListBinaryNum.py
@@ -1,3 +1,5 @@
+from ByteNode import ByteNode
+import math
 #LSB = Least Significant Bit
 #MSB = Most Significant Bit
 
@@ -9,9 +11,18 @@ class LinkedListBinaryNum:
         '''
         if not isinstance(num, int):
             raise ValueError("Error! Input must be integer!")
-        if num <= 0:
+        if num < 0:
             raise ValueError("Error! Input must be positive!")
+        binary = bin(num)[2:]
+        remnant = len(binary) % 8
+        if remnant != 0:
+            my_str = binary.zfill(((8 - remnant) + len(binary)))
+        else:
+            my_str = binary
+        self.head = ByteNode(my_str[:8])
+        size = ((8 - remnant) + len(binary))/ 8
+
 
     def add_MSB(self, byte:  str):
-        pass
+        pass 
         
