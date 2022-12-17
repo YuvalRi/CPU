f16256f073417d18ab72f8b042fe862862583341
Yuval Ritsker <yuvalritsker@gmail.com>
2022-12-17 18:56:49 +0200

    finished init function

--- source/LinkedListBinaryNum.py
+++ source/LinkedListBinaryNum.py
@@ -15,14 +15,35 @@ class LinkedListBinaryNum:
             raise ValueError("Error! Input must be positive!")
         binary = bin(num)[2:]
         remnant = len(binary) % 8
-        if remnant != 0:
-            my_str = binary.zfill(((8 - remnant) + len(binary)))
-        else:
-            my_str = binary
+        my_str = binary.zfill(((8 - remnant) + len(binary)))
         self.head = ByteNode(my_str[:8])
-        size = ((8 - remnant) + len(binary))/ 8
+        self.size = (len(my_str))/8
+        curr_pos = self.head
+        for j in range(self.size - 1):
+            pos = ByteNode(my_str[8*j:8*j + 8])
+            curr_pos.next = pos
+            curr_pos = pos
+        
+
+        # my_str2 = [my_str[i:i+8] for i in range(0, len(my_str), 8)]
+        # for ele in my_str2[:len(my_str2)-1]:
+        #     print(ByteNode(ele), end =" ")
+        # print(f"{ByteNode(my_str2[len(my_str2)-1])}None")
 
 
     def add_MSB(self, byte:  str):
         pass 
-        
+
+
+if __name__ == "__main__":
+    # binary = bin(256)[2:]
+    # remnant = len(binary) % 8
+    # my_str = binary.zfill(((8 - remnant) + len(binary)))
+    # head = ByteNode(my_str[:8])
+    # size = (len(my_str)) / 8 
+    # my_str2 = [my_str[i:i+8] for i in range(0, len(my_str), 8)]
+    # for ele in my_str2[:len(my_str2)-1]:
+    #     print(ByteNode(ele), end =" ")
+    # print(f"{ByteNode(my_str2[len(my_str2)-1])}None")
+    # print(head)
+    # print(size)
